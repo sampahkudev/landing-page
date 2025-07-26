dc091a31fb46e7fd6969d2d2c659370c3020813a
agung6544 <agung.nainggolan8008@gmail.com>
2025-07-27 03:51:20 +0700

    change url

--- js/url.js
+++ js/url.js
@@ -1,2 +1,2 @@
-export let authURL = "http://127.0.0.1:8011/api/auth";
+export let authURL = "https://api.sampahku.herobuxx.me/api/auth";
 export let regURL = "https://api.sampahku.herobuxx.me/api/register";
